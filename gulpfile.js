225ea6e43fc4c22e799b9566005f1258498a0576
Andy Fryman <andyfryman@users.noreply.github.com>
2020-02-15 17:21:24 +0300

    Add favicon

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var fileinclude = require('gulp-file-include');
 var replace = require('gulp-replace');
 var htmlmin = require('gulp-htmlmin');
 var spritesmith = require('gulp.spritesmith');
+var realFavicon = require('gulp-real-favicon');
+var fs = require('fs');
 
 gulp.task('header', function () {
     return gulp.src('./src/header/image/*')
@@ -75,6 +77,73 @@ gulp.task('sprite', function () {
     return spriteData.pipe(gulp.dest('./src/leader/sprite/'));
 });
 
+var FAVICON_DATA_FILE = './src/favicon/faviconData.json';
+gulp.task('favicon', function (done) {
+    realFavicon.generateFavicon({
+        masterPicture: './src/favicon/image/logo.svg',
+        dest: './src/favicon/icon/',
+        iconsPath: '/',
+        design: {
+            ios: {
+                pictureAspect: 'backgroundAndMargin',
+                backgroundColor: '#ffffff',
+                margin: '14%',
+                assets: {
+                    ios6AndPriorIcons: true,
+                    ios7AndLaterIcons: true,
+                    precomposedIcons: false,
+                    declareOnlyDefaultIcon: true
+                }
+            },
+            desktopBrowser: {
+                design: 'raw'
+            },
+            windows: {
+                pictureAspect: 'whiteSilhouette',
+                backgroundColor: '#da532c',
+                onConflict: 'override',
+                assets: {
+                    windows80Ie10Tile: true,
+                    windows10Ie11EdgeTiles: {
+                        small: false,
+                        medium: true,
+                        big: false,
+                        rectangle: false
+                    }
+                }
+            },
+            androidChrome: {
+                pictureAspect: 'noChange',
+                themeColor: '#ffffff',
+                manifest: {
+                    display: 'standalone',
+                    orientation: 'notSet',
+                    onConflict: 'override',
+                    declared: true
+                },
+                assets: {
+                    legacyIcon: false,
+                    lowResolutionIcons: false
+                }
+            },
+            safariPinnedTab: {
+                pictureAspect: 'silhouette',
+                themeColor: '#5bbad5'
+            }
+        },
+        settings: {
+            scalingAlgorithm: 'Mitchell',
+            errorOnImageTooSmall: false,
+            readmeFile: false,
+            htmlCodeFile: false,
+            usePathAsIs: false
+        },
+        markupFile: FAVICON_DATA_FILE
+    }, function () {
+        done();
+    });
+});
+
 var replaceHeader = `<!doctype html>
 <html lang="en">
 
@@ -104,6 +173,7 @@ gulp.task('index', function (cb) {
         .pipe(fileinclude())
         .pipe(replace(replaceHeader, ''))
         .pipe(replace(replaceFooter, ''))
+        .pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
         .pipe(htmlmin({
             minifyCSS: true,
             minifyJS: true,
@@ -137,6 +207,9 @@ gulp.task('index', function (cb) {
         .pipe(gulp.dest('./docs/shape/'));
 
     gulp.src('./src/facebook/facebook.png')
+        .pipe(gulp.dest('./docs/'));
+
+    gulp.src('./src/favicon/icon/*')
         .pipe(gulp.dest('./docs/'));
 
     cb();
